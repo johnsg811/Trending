5ffb691e6dcf0b23f2570aac317704244dde6026
johnspep <johnspep@hotmail.com>
2015-04-06 12:20:19 +0100

    Actual initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Trending
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(42, 47);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 422);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(316, 47);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(766, 433);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.webBrowser1_Navigating);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(205, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(443, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "http://rss.news.yahoo.com/rss/topstories";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(654, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1144, 522);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace Trending
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Feed()
+        {
+            try
+            {
+                // get an xpath navigator   
+                DataTable dt = new DataTable();
+                dt.Columns.Add("Title", typeof(string));
+                dt.Columns.Add("Link", typeof(string));
+                try
+                {
+
+                    
+                  XmlDocument RSSXml = new XmlDocument();
+                  RSSXml.Load(textBox1.Text);
+                  this.Cursor = Cursors.Default;
+                 XmlNodeList RSSNodeList = RSSXml.SelectNodes("rss/channel/item");
+                XmlNode RSSDesc = RSSXml.SelectSingleNode("rss/channel/title");
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (XmlNode RSSNode in RSSNodeList)
+                {
+                    int i=0;
+                    XmlNode RSSSubNode;
+                    RSSSubNode = RSSNode.SelectSingleNode("title");
+                    string title = RSSSubNode != null ? RSSSubNode.InnerText : "";
+
+                    RSSSubNode = RSSNode.SelectSingleNode("link");
+                    string link = RSSSubNode != null ? RSSSubNode.InnerText : "";
+
+                    RSSSubNode = RSSNode.SelectSingleNode("description");
+                    string desc = RSSSubNode != null ? RSSSubNode.InnerText : "";
+
+                    DataRow dr = dt.NewRow();
+                    dr["Title"] = title;
+                    dr["Link"] = link;
+                    dt.Rows.Add(dr);
+                    
+
+                }
+                dataGridView1.DataSource = dt;
+                   
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "RSS Feed Load Error");
+                }
+
+                // restore the cursor
+                this.Cursor = Cursors.Default;
+               // tvwRss.ExpandAll();
+
+            }
+            catch (Exception ex2)
+            {
+                // snitch
+                MessageBox.Show(ex2.ToString(), "RSS Feed Initialization Failure");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Feed();
+        }
+
+        private void webBrowser1_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int rowIndex = e.RowIndex;
+            DataGridViewRow row = dataGridView1.Rows[rowIndex];
+            webBrowser1.Navigate(row.Cells[1].Value.ToString());
+        }
+    }
+}
