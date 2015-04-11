71821c6e128e758430e282f41fd612e61f423aed
Johns George <johnspep@hotmail.com>
2015-04-11 23:20:03 +0100

    AboutUs Form update

--- frmHelp.Designer.cs
+++ frmHelp.Designer.cs
@@ -32,6 +32,7 @@
             this.txt1 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lbl1
@@ -47,7 +48,7 @@
             // 
             this.txt1.Location = new System.Drawing.Point(127, 51);
             this.txt1.Name = "txt1";
-            this.txt1.Size = new System.Drawing.Size(310, 20);
+            this.txt1.Size = new System.Drawing.Size(322, 20);
             this.txt1.TabIndex = 1;
             this.txt1.Text = "Select Any News Websites given on the right side to read news";
             // 
@@ -55,7 +56,7 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(127, 88);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(310, 20);
+            this.textBox1.Size = new System.Drawing.Size(322, 20);
             this.textBox1.TabIndex = 2;
             this.textBox1.Text = "User can click on any news link to read the full news below";
             // 
@@ -63,14 +64,24 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(127, 129);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(310, 20);
+            this.textBox2.Size = new System.Drawing.Size(322, 20);
             this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "User can add news websites by giving url of rss in ADD FEED";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(127, 173);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(322, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "User can add news websites by giving url of rss in ADD FEED";
             // 
             // frmHelp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(475, 349);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.txt1);
@@ -88,5 +99,6 @@
         private System.Windows.Forms.TextBox txt1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
